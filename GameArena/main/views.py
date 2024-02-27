7e8c510d6396bb4b393d03b692f6f72435b4e044
rahaf <rahaf09876509@gmail.com>
2024-02-27 03:24:06 +0300

    GameArena

---
+++ GameArena/main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+
+def home_page(request:HttpRequest):
+ 
+ return render(request,"main/index.html")
+# Create your views here.
