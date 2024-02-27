2080fc0f6af8fa961f98eaf6ae1be104e5004e9d
Fai <faiabdulla123@gmail.com>
2024-02-27 04:12:13 +0300

    DJANGO-APPS

---
+++ GameArena/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
+def home_page(request:HttpRequest):
+
+    return render(request, "main/home_page.html")
+
